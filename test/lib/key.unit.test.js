d931349bfc8d100816adffa79c9c5811b3615566
Jeremie Miller <jeremie@jabber.org>
2013-01-10 14:13:27 -0600

    test the mem and fs backends for key

---
+++ test/lib/key.unit.test.js
@@ -0,0 +1,56 @@
+var assert = require("assert");
+var key = require("key");
+var store;
+
+describe("Key", function() {
+  describe("mem", function() {
+    before(function (done) {
+      key.store('mem', {}, function(err, mem){
+        store = mem;
+        done();
+      });
+    });
+    it("set", function(done) {
+      store.set("foo", new Buffer("bar"), done);
+    });
+    it("gets", function(done) {
+      store.get("foo", function(err, val){
+        assert.ifError(err);
+        assert.equal("bar", val.toString());
+        done();
+      });
+    });
+    it("fails", function(done) {
+      store.get("bar", function(err, val){
+        assert(err);
+        done();
+      });
+    });
+  });
+
+  describe("fs", function() {
+    before(function (done) {
+      key.store('fs', {base_path:'/tmp/kvtest'}, function(err, mem){
+        store = mem;
+        done();
+      });
+    });
+    it("set", function(done) {
+      store.set("foo", new Buffer("bar"), done);
+    });
+    it("gets", function(done) {
+      store.get("foo", function(err, val){
+        assert.ifError(err);
+        assert.equal("bar", val.toString());
+        done();
+      });
+    });
+    it("fails", function(done) {
+      store.get("bar", function(err, val){
+        assert(err);
+        done();
+      });
+    });
+  });
+
+});
